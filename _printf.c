7b568b9762d144190ff6f5eca3c3478f2ac3022a
DocMthetdo <doctormthethwa@gmail.com>
2023-08-22 07:12:17 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,45 +1,66 @@
 #include "main.h"
-#include "stdarg.h"
-#include "stdio.h"
 /**
- * _printf - a function that behaves like printf
- * @format: format string
- * Return: number of characters printed
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
+/**
+ * _printf - behaves like a printf finction
+ * @format: The format string
+ * ...
+ *
+ * Return: The number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	int i;
-
 
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	while (*format)
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			putchar(format[i]);
-			count++;
-			continue;
-		}
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+				{
+					char *s = va_arg(args, char *);
 
-		i++;
-
-		switch (format[i])
-		{
-			case 'd':
-			case 'i':
-				printf("%d", va_arg(args, int));
+					while (*s)
+					{
+						_putchar(*s++);
+						count++;
+					}
+				}
+				break;
+				case '%':
+				_putchar('%');
 				count++;
 				break;
-			default:
-				putchar('%');
-				putchar(format[i]);
-				count += 2;
+				default:
 				break;
+			}
 		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
 	}
 
 	va_end(args);
